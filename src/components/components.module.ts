6a10ae4de732a40ed05bda8cd2bba273fb071021
Ezequiel Masciarelli <ezequiel.masciarelli@despegar.com>
2019-06-01 19:47:38 -0300

    se borra el componente que no se usa

--- src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { PlayerNameComponent } from './player-name/player-name';
-@NgModule({
-	declarations: [PlayerNameComponent],
-	imports: [],
-	exports: [PlayerNameComponent]
-})
-export class ComponentsModule {}
