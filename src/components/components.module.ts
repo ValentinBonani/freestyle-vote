f781a2095d5a94ba9291b7caaa0cb2cf6723e4eb
ValentinBonani <valentin.bonani@gmail.com>
2019-06-01 19:33:29 -0300

    freestyle-000 add component player-name

---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { PlayerNameComponent } from './player-name/player-name';
+@NgModule({
+	declarations: [PlayerNameComponent],
+	imports: [],
+	exports: [PlayerNameComponent]
+})
+export class ComponentsModule {}
---
+++ src/components/player-name/player-name.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+/**
+ * Generated class for the PlayerNameComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'player-name',
+  templateUrl: 'player-name.html'
+})
+export class PlayerNameComponent {
+
+  text: string;
+
+  constructor() {
+    console.log('Hello PlayerNameComponent Component');
+    this.text = 'Hello World';
+  }
+
+}
